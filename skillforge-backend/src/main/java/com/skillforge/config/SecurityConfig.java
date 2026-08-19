b28c9c978bb59665590236ed311415f1d56b41c1
Aaditya Pathak <aadityapathak18800@gmail.com>
2026-08-19 17:50:36 +0530

    Configure frontend URL with backend for deployment

--- skillforge-backend/src/main/java/com/skillforge/config/SecurityConfig.java
+++ skillforge-backend/src/main/java/com/skillforge/config/SecurityConfig.java
@@ -71,8 +71,10 @@ public class SecurityConfig {
 		
 		CorsConfiguration configuration = new CorsConfiguration();
 		
-		configuration.setAllowedOrigins(
-				List.of("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of(
+			    "http://localhost:5173",
+			    "https://skillforge-frontend-orcin.vercel.app"
+		));
 		
 		configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
